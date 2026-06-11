e02dbd9df3e68da3459a8da2ea2c4cd8ee4752fe
adityapritam <adityapritam151@gmail.com>
2026-06-11 23:06:51 +0530

    Added confirmation message

--- Random_Password_Generator/script.js
+++ Random_Password_Generator/script.js
@@ -2,6 +2,7 @@ let pwd=document.querySelector("#text");
 let set="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
 let s="";
 
+let confirm=document.querySelector(".confirm");
 let submit=document.getElementById("submit");
 submit.addEventListener("click",function(){
     for(let i=1;i<=12;i++){
@@ -9,10 +10,24 @@ submit.addEventListener("click",function(){
     }
     pwd.textContent=`${s}`;
     s="";
+    confirm.style.animation="slideout 1s forwards";
 });
 
 let copy=document.getElementById("copy");
 copy.addEventListener("click",function(){
+    if((pwd.textContent)==""){
+        return false;
+    }
     navigator.clipboard.writeText(pwd.textContent);
-    alert("Password copied!");
+    confirm.style.visibility="visible";
+    confirm.style.animation="slidein 0.39s forwards";
+    setTimeout(function(){
+    confirm.style.animation="slideout 1s forwards";
+    },2*1000)
+});
+
+
+let close=document.getElementById("cross");
+close.addEventListener("click",function(){
+    confirm.style.animation="slideout 1s forwards";
 });
